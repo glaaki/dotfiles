c1021e306736706cec3c520d86756044508f185f
glaaki <dreadpiratesean@gmail.com>
2017-10-27 13:49:05 -0500

    added more symlinks

--- setup.sh
+++ setup.sh
@@ -3,8 +3,15 @@
 conf_dir = $HOME/.config
 dots_dir = $HOME/git/dotfiles
 backup_dir = /tmp/config_backup
+
 mkdir $backup_dir
 
 # this lists the dotfiles dir, moves anything that already exists into a backup folder, then symlinks everything.
 ls -1 $dots_dir | xargs -i{} mv $conf_dir/{} $backup_dir/ || true && ln -s $dots_dir/* $conf_dir
 
+ln -sf $dots_dir/.vimrc $HOME
+ln -sf $dots_dir/.zshrc $HOME
+ln -sf $dots_dir/.tmux.conf $HOME
+ln -sf $dots_dir/.tmux.conf.local $HOME
+ln -sf $dots_dir/.extra $HOME
+ln -sf $dots_dir/.aliases $HOME
