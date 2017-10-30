5418fcdd80df227428e6659e30b25241908403ca
glaaki <dreadpiratesean@gmail.com>
2017-10-30 08:54:23 -0500

    fixed setup script so it actually works

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-conf_dir = $HOME/.config
-dots_dir = $HOME/git/dotfiles
-backup_dir = /tmp/config_backup
+conf_dir=$HOME/.config
+dots_dir=$HOME/git/dotfiles
+backup_dir=/tmp/config_backup.$(date +%Y-%m-%d.%S)
 
-mkdir $backup_dir
+mkdir -p $backup_dir
 
 # this lists the dotfiles dir, moves anything that already exists into a backup folder, then symlinks everything.
-ls -1 $dots_dir | xargs -i{} mv $conf_dir/{} $backup_dir/ || true && ln -s $dots_dir/* $conf_dir
+ls -1 $dots_dir/config | xargs -i{} mv $conf_dir/{} $backup_dir/ || true && ln -s $dots_dir/config/* $conf_dir
 
 ln -sf $dots_dir/.vimrc $HOME
 ln -sf $dots_dir/.zshrc $HOME
